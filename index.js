481f03a815213ff3b88bda04ad63767dfa590bcb
Shelby Lee <slee337@uw.edu>
2022-08-18 15:48:11 -0600

    some test stuff

--- index.js
+++ index.js
@@ -8,11 +8,25 @@
 
 // reset favorites
 
-let testImg = document.getElementById('test-img')
+let testImg = document.getElementById('testImg')
+
+let currentDawgBreed
 
 fetch(`https://dog.ceo/api/breeds/image/random`)
 .then(resp => resp.json())
 .then(dawgs => {
-    console.log(dawgs)
+    //console.log(dawgs)
     testImg.src = dawgs.message
+    getDawgBreed(dawgs)
 })
+
+function getDawgBreed(dawgs) {
+    let dawgBreed = []
+    rawURLArray = dawgs.message.substring(30).split('')
+    rawURLArray.forEach(char => {
+        while (char != '/') {
+            dawgBreed.push(char)
+        } 
+    })
+    console.log(dawgBreed)
+}
