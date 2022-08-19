3c5ee81a7ba2ac3930a671e0da14f708963fb0cc
Shelby Lee <slee337@uw.edu>
2022-08-19 12:56:59 -0600

    guess protoype done

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 let mainImg = document.getElementById('mainImage')
+let guessForm = document.getElementById('guessInput')
+let scoreDisplay = document.getElementById('scoreDisplay')
+let previousGuess = document.getElementById('previousGuess')
+let previousAnswer = document.getElementById('previousAnswer')
 
+// initialize variables for the current dawg on the screen
 let currentDawgBreed
 let currentDawgURL
-let previousDawgBreed
 
+// initialize guessing variables
 let breedGuess
+let currentScore = 0;
+
+// data so can save curret dawg as favorite
+let previousDawgBreed
 let previousBreedGuess
 
+// initializes the displays to be empty
+scoreDisplay.textContent = `Your Score: ${currentScore}`
+previousGuess.textContent = `Your Last Guess: `
+previousAnswer.textContent = `The Correct Answer: `
+
+
+
 fetch(`https://dog.ceo/api/breeds/image/random`)
-.then(resp => resp.json())
-.then(data => {
-    mainImg.src = data.message
-    getDawgBreed(data)
-})
+    .then(resp => resp.json())
+    .then(data => {
+        mainImg.src = data.message
+        getDawgBreed(data)
+        guessingForm(currentDawgBreed)
+    })
 
 // this function takes in the data obj, selects the url, and then dissects it down to a dawg breed string in plain english
 function getDawgBreed(data) {
@@ -30,19 +47,38 @@ function getDawgBreed(data) {
 
 // GAME
 
-guessingForm(currentDawgBreed)
+// breed guessing funtion!
+function guessingForm(){
+    // event listener for user input
+    guessForm.addEventListener('submit', e => {
+        e.preventDefault()
+        breedGuess = e.target['guessText'].value
+        console.log(breedGuess)
+        guessForm.reset()
+    
+    // if the breed is right, the current score is incremented by 1 and displayed
+    if (breedGuess == currentDawgBreed) {
+        currentScore ++
+        scoreDisplay.textContent = `Your Score: ${currentScore}`
+    }
 
-// breed input for guess
-function guessingForm(currentDawgBreed){
-    console.log(currentDawgBreed)
-
+    // calls function to build out past game history section
+    previousRoundData(breedGuess,currentDawgBreed)
+    })
 }
 
-// if guess is right or not
-
-// score updater
-
 // previous round data
+function previousRoundData(breedGuess,currentDawgBreed) {
+    // logs the current as previous
+    previousBreedGuess = breedGuess
+    previousDawgBreed = currentDawgBreed
+    previousGuess.textContent = `Your Last Guess: ${previousBreedGuess}`
+    previousAnswer.textContent = `The Correct Answer: ${previousDawgBreed}`
+}
+
+
+
+
 
 // FAVORITES
 
