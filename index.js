27232f422d8577fe424ed592d42391e4c78b019c
Shelby Lee <slee337@uw.edu>
2022-08-19 11:47:31 -0600

    trying again

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-let testImg = document.getElementById('testImg')
+let mainImg = document.getElementById('mainImage')
 
 let currentDawgBreed
 
 fetch(`https://dog.ceo/api/breeds/image/random`)
 .then(resp => resp.json())
 .then(data => {
-    testImg.src = data.message
+    mainImg.src = data.message
     getDawgBreed(data)
 })
 
 // this function takes in the data obj, selects the url, and then dissects it down to a dawg breed string in plain english
 function getDawgBreed(data) {
-    let dawgBreed = data.message.substring(30) // knocks off the http stuff that is always there
-    let indexAtEndOfBreed = dawgBreed.indexOf('/',0) // figures out where the breed ends
-    dawgBreed = dawgBreed.substring(0,indexAtEndOfBreed)
-    console.log(dawgBreed)
+    currentDawgBreed = data.message.substring(30) // knocks off the http stuff that is always there
+    let indexAtEndOfBreed = currentDawgBreed.indexOf('/',0) // figures out where the breed ends
+    currentDawgBreed = currentDawgBreed.substring(0,indexAtEndOfBreed)
 
-    if (dawgBreed.includes('-')) {
-        let indexOfDash = dawgBreed.indexOf('-')
-        dawgBreed = `${dawgBreed.substring((indexOfDash+1))} ${dawgBreed.substring(0,indexOfDash)}` 
+    if (currentDawgBreed.includes('-')) {
+        let indexOfDash = currentDawgBreed.indexOf('-')
+        currentDawgBreed = `${currentDawgBreed.substring((indexOfDash+1))} ${currentDawgBreed.substring(0,indexOfDash)}` 
     }
-    console.log(dawgBreed)
+    console.log(currentDawgBreed)
+    return(currentDawgBreed)
 }
